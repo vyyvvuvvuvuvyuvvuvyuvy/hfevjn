abe7d7d82c6beef9ea3fd47a19993bbc1198ffbc
vyyvvuvvuvuvyuvvuvyuvy <157741817+vyyvvuvvuvuvyuvvuvyuvy@users.noreply.github.com>
2025-03-19 11:55:41 -0500

    Create spawnereggs.java

---
+++ spawnereggs.java
@@ -0,0 +1,9 @@
+// Granny Spawn Egg
+public static final Item GRANNY_SPAWN_EGG = new ItemSpawnEgg(GrannyEntity.TYPE, 0xFFFFFF, 0x000000)
+    .setUnlocalizedName("granny_spawn_egg")
+    .setCreativeTab(CreativeTabs.MISC);
+
+// Slendrina Spawn Egg
+public static final Item SLENDRINA_SPAWN_EGG = new ItemSpawnEgg(SlendrinaEntity.TYPE, 0x000000, 0xFF0000)
+    .setUnlocalizedName("slendrina_spawn_egg")
+    .setCreativeTab(CreativeTabs.MISC);
